ffad26fba4147b628a9fa2f0cd4c2ffaf127da5b
kumkumsingh <kumkumsingh.r@gmail.com>
2019-10-28 16:41:19 +0100

    created user model,db.js and did the setup of index.js and router

---
+++ User/router.js
@@ -0,0 +1,6 @@
+const {Router} = require('express')
+const User = require('./model')
+
+const router = new Router()
+
+module.exports = router
---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const userRouter = require('./User/router')
+
+const app = express()
+
+const corsMiddleware = cors()
+app.use(corsMiddleware)
+
+const parserMiddleware = bodyParser.json()
+app.use(parserMiddleware)
+
+app.use(userRouter)
+
+const port = 4000;
+app.listen(port, () => console.log(`Listening to port ${port}`))
